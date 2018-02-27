c4b03c8c6670195701a2b731b328aba924b6deff
Lahiru-Weerasekara <weerasekara1121@gmial.com>
2018-02-27 14:57:04 +0530

    Emit new message on create meassage

--- public/js/index.js
+++ public/js/index.js
@@ -8,10 +8,10 @@ socket.on('connect', function () {
     //     text: 'Hey, this is'
     // });
 
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text: 'Yup, that works for me'
-    });
+    // socket.emit('createMessage', {
+    //     from: 'Andrew',
+    //     text: 'Yup, that works for me'
+    // });
 
 });
 
--- server/server.js
+++ server/server.js
@@ -20,11 +20,11 @@ io.on('connection', (socket) => {
     //     createdAt: 123
     // });
 
-    socket.emit('newMessage', {
+    /*socket.emit('newMessage', {
         from: 'mike',
         text: 'sgjddsfu',
         createdAt: 123123
-    });
+    });*/
 
     // socket.on('createEmail', (newEmail) => {
     //     console.log('createEmail', newEmail);
@@ -32,6 +32,11 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
